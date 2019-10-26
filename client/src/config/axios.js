55d7331c1955e1dd7e94bf3d449c8a31c4ab943f
Aman N <aman.narayan1998@gmail.com>
2019-10-26 17:06:11 +0530

    changed base url

--- client/src/config/axios.js
+++ client/src/config/axios.js
@@ -1,8 +1,8 @@
 import Axios from 'axios'
 
-const url = process.env.URL || 'http://localhost:3001'
+// const url = process.env.URL || 'http://localhost:3001'
 const axios = Axios.create({
-    baseURL: url
+    baseURL: '/'
 })
 
 export default axios
