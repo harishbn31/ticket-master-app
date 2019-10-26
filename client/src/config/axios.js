51bc7f7f827cfb59a0ab01bd953c0f5ae3523b9c
Aman N <aman.narayan1998@gmail.com>
2019-10-26 17:41:04 +0530

    fixed bugs

--- client/src/config/axios.js
+++ client/src/config/axios.js
@@ -2,7 +2,7 @@ import Axios from 'axios'
 
 // const url = process.env.URL || 'http://localhost:3001'
 const axios = Axios.create({
-    baseURL: 'http://localhost:3001'
+    baseURL: '/'
 })
 
 export default axios
