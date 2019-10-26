9232c3298ff33ea88a55d986f0801938005e6f9a
Aman N <aman.narayan1998@gmail.com>
2019-10-26 15:52:59 +0530

    Made changes to db

--- config/database.js
+++ config/database.js
@@ -1,8 +1,9 @@
 const mongoose = require('mongoose')
 
 mongoose.Promise = global.Promise
-
-mongoose.connect('mongodb+srv://aman1706:60718991Zebram@cluster0-8t0yk.mongodb.net/test?retryWrites=true&w=majority',{useNewUrlParser: true})
+// 'mongodb+srv://aman1706:60718991Zebram@cluster0-8t0yk.mongodb.net/test?retryWrites=true&w=majority'
+const CONNECTION_URI= process.env.MONGODB_URI || "mongodb://localhost:27017/ticket-master-bk"
+mongoose.connect(CONNECTION_URI,{useNewUrlParser: true})
     .then(()=>{
         console.log('succesfully connected to db')
     })
